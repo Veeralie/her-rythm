026786b583ba630ab310937cebcaa42f2a520332
Veeralie <beranovaleriejane@gmail.com>
2026-04-08 17:51:16 +0100

    Update CycleWellnessPage.jsx

--- CycleWellnessPage.jsx
+++ CycleWellnessPage.jsx
@@ -659,7 +659,6 @@ export default function CycleWellnessPage() {
                   const fertile = item.fertile ? "shadow-[0_0_0_1px_rgba(255,209,102,0.45)]" : "";
                   const ovulation = item.ovulation ? "after:absolute after:bottom-1 after:left-1/2 after:h-1.5 after:w-1.5 after:-translate-x-1/2 after:rounded-full after:bg-white" : "";
                   const selected = hasUserSelectedDate && dateKey(item.date) === dateKey(selectedDate) ? "ring-2 ring-[#d3ae91] shadow-lg shadow-[#d3ae91]/20" : "ring-1 ring-white/5";
-                
                   return (
                     <button
                       key={`${item.day}-${index}`}
